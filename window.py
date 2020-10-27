d7c4626cf62d7070299d2e2909dbc7d120ec6425
Luke Polson <47739849+Luke-Polson@users.noreply.github.com>
2020-10-28 09:44:48 +1300

    Added option to change stock when running. Bug fixes.

--- window.py
+++ window.py
@@ -10,45 +10,43 @@ class Window:
 
     def __init__(self):
 
-        root = tk.Tk()
-        root.withdraw()
+        # initialise root window
+        self.root = tk.Tk()
+        self.root.withdraw()
 
         self.button1 = None
         self.ticker_entry = None
         self.ticker_label = None
 
-        self.welcome_window = tk.Toplevel(root)
+        # welcome window asks for a ticker as input
+        self.welcome_window = tk.Toplevel(self.root)
         self.welcome_window.title("Please enter a valid ticker")
 
-        self.main_window = tk.Toplevel(root)
+        # main window to display stock info
+        self.main_window = tk.Toplevel(self.root)
         self.main_window.title("Stock Visualiser")
 
         self.populate_welcome()
 
         self.main_window.withdraw()
 
-        root.mainloop()
+        self.root.mainloop()
 
     def populate_welcome(self):
 
-        self.ticker_label = tk.Label(self.welcome_window,
-                                     text="Enter a stock ticker, eg 'AAPL'",
-                                     font=('',20),
-                                     padx=10,
-                                     pady=10)
+
+        self.ticker_label = tk.Label(self.welcome_window, text="Enter a stock ticker, eg 'AAPL'",
+                                     font=('', 20), padx=10, pady=10)
 
         self.ticker_label.grid(row=0,column=0)
 
-        self.button1 = tk.Button(self.welcome_window, text='Go', width=20, command=self.goto_main)
+        self.button1 = tk.Button(self.welcome_window, text='Go', width=20, pady=7, command=self.goto_main)
         self.ticker_entry = tk.Entry(self.welcome_window, textvariable=tk.StringVar)
 
-        self.button1.grid(row=2,column=0)
-        self.ticker_entry.grid(row=1, column=0)
+        self.button1.grid(row=2,column=0, pady=10)
+        self.ticker_entry.grid(row=1, column=0, pady=10)
 
     def goto_main(self):
-
-        # TODO: embed plot within main window
-        # TODO: calculate info including volatility, percentage increases, etc
 
         entered_ticker = self.ticker_entry.get()
 
@@ -56,26 +54,40 @@ class Window:
 
         self.populate_main(data, entered_ticker)
 
-        self.welcome_window.destroy()
+        self.welcome_window.withdraw()
         self.main_window.deiconify()
 
     def populate_main(self, data, ticker):
+
+        change_stock = tk.Button(self.main_window, text="Change", command=self.change_stock)
+        change_stock.pack()
 
         fig = plt.Figure()
         ax = fig.add_subplot(111)
         chart_type = FigureCanvasTkAgg(fig, self.main_window)
         chart_type.get_tk_widget().pack()
 
-        data['Adj Close'].plot(title="Stock Price: " + ticker, ax=ax)
+        # plot the closing prices for the past year
+        data['Adj Close'][-365:].plot(title="Stock Price: " + ticker, ax=ax)
+
+        # closing price from the day before, and closing price from the current day
+        opening = tk.Label(self.main_window, text="Opening: ${:.3f}".format(data['Adj Close'][-2]))
+        closing = tk.Label(self.main_window, text="Closing: ${:.3f}".format(data['Adj Close'][-1]))
+
+        opening.pack(side=tk.LEFT)
+        closing.pack(side=tk.LEFT)
 
         # calculate standard deviation for the stock price over last 1000 days
         sigma = np.std([price for price in data['Adj Close'][-1000:]])
 
-        self.volatility_label = tk.Label(self.main_window, text="Volatility: ${:.3f}".format(sigma), padx=10)
-        self.volatility_label.pack(side=tk.LEFT)
+        volatility_label = tk.Label(self.main_window, text="Volatility: ${:.3f}".format(sigma), padx=10)
+        volatility_label.pack(side=tk.LEFT)
 
+    def change_stock(self):
 
+        self.root.destroy()
 
+        self.__init__()
 
 
 
